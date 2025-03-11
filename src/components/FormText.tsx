59244af739b6f5301ff72e813119f621cabd9f77
ildopetrijunior <ildopetrijunior@gmail.com>
2025-03-11 17:23:57 -0300

    update formtext

--- src/components/FormText.tsx
+++ src/components/FormText.tsx
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useState, useEffect } from "react"
 import { Text, TextInput, TextInputProps, useTheme } from "react-native-paper"
 import { DimensionValue, TextInput as OriginalInput, View } from "react-native"
 import { FormikErrors, FormikTouched } from "formik"
@@ -6,6 +6,8 @@ import { mask as masked } from "react-native-mask-text"
 import { LabeledComponent } from "./LabeledComponent"
 import lodash from "lodash"
 import { scale } from "../tools/scale"
+import { colors } from "../style/colors"
+import { ios } from "../tools/platforms"
 
 export interface FormTextProps extends TextInputProps {
     name: string
@@ -27,12 +29,24 @@ export interface FormTextProps extends TextInputProps {
     required?: boolean
     forced_error?: string
     innerLabel?: boolean
+    password?: boolean
 }
 
 export const FormText = React.forwardRef<React.ElementRef<typeof OriginalInput>, FormTextProps>((props, ref) => {
     const theme = useTheme()
     const error = !!props.forced_error || !!(lodash.get(props.formik.touched, props.name) && lodash.get(props.formik.errors, props.name))
     const error_text = props.forced_error || (lodash.get(props.formik.errors, props.name) as string)
+
+    const [inputValue, setInputValue] = useState<string>(lodash.get(props.formik.values, props.name) || "")
+    const [hiddenPassword, setHiddenPassword] = props.password ? useState(true) : useState(false)
+
+    // Sincroniza o estado local com o valor do Formik caso este seja atualizado externamente
+    useEffect(() => {
+        const formikValue = lodash.get(props.formik.values, props.name) || ""
+        if (formikValue !== inputValue) {
+            setInputValue(formikValue)
+        }
+    }, [props.formik.values, props.name])
 
     return (
         <View style={{ width: props.width, flex: props.flex }}>
@@ -47,6 +61,12 @@ export const FormText = React.forwardRef<React.ElementRef<typeof OriginalInput>,
                         right={
                             props.right ? (
                                 props.right
+                            ) : props.password ? (
+                                <TextInput.Icon
+                                    icon={!hiddenPassword ? "eye-off" : "eye"}
+                                    color={error ? theme.colors.error : colors.error}
+                                    onPress={() => setHiddenPassword((value) => !value)}
+                                />
                             ) : error ? (
                                 <TextInput.Icon icon={"alert-circle"} color={theme.colors.error} size={scale(24)} />
                             ) : undefined
@@ -66,20 +86,35 @@ export const FormText = React.forwardRef<React.ElementRef<typeof OriginalInput>,
                         returnKeyType={props.returnKeyType || "next"}
                         error={error}
                         value={
-                            props.value ||
-                            (props.mask
-                                ? masked(lodash.get(props.formik.values, props.name), props.mask)
-                                : lodash.get(props.formik.values, props.name) || "")
+                            ios
+                                ? props.mask
+                                    ? masked(inputValue, props.mask)
+                                    : inputValue
+                                : props.value ||
+                                  (props.mask
+                                      ? masked(lodash.get(props.formik.values, props.name), props.mask)
+                                      : lodash.get(props.formik.values, props.name) || "")
                         }
                         // @ts-ignore
+
                         onChangeText={
-                            props.onChangeText ||
-                            (props.mask
-                                ? (value) => props.formik.setFieldValue(props.name, masked(value, props.mask))
-                                : // @ts-ignore
-                                  props.formik.handleChange(props.name))
+                            ios
+                                ? (value: string) => {
+                                      setInputValue(value)
+                                      if (props.mask) {
+                                          props.formik.setFieldValue(props.name, masked(value, props.mask))
+                                      } else {
+                                          props.formik.setFieldValue(props.name, value)
+                                      }
+                                  }
+                                : props.onChangeText ||
+                                  (props.mask
+                                      ? (value) => props.formik.setFieldValue(props.name, masked(value, props.mask))
+                                      : // @ts-ignore
+                                        props.formik.handleChange(props.name))
                         }
                         onBlur={props.onBlur || props.formik.handleBlur(props.name)}
+                        secureTextEntry={hiddenPassword}
                     />
                 }
             />
